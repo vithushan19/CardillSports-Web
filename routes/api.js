1a2d41d8d4f9c49b7a71fdaaf6acdad21a75d1ef
Vithushan Nama <vithushan@spotify.com>
2016-06-01 10:49:56 -0400

    spacing on api

--- routes/api.js
+++ routes/api.js
@@ -3,43 +3,39 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 var PlayerRankingSchema = new mongoose.Schema({
-  1: Object,
-  2: Object,
-  3: Object,
-  4: Object,
-  5: Object,
-  6: Object,
-  7: Object,
-  8: Object,
-  9: Object,
-  10: Object,
-  11: Object,
-  12: Object
+    1: Object,
+    2: Object,
+    3: Object,
+    4: Object,
+    5: Object,
+    6: Object,
+    7: Object,
+    8: Object,
+    9: Object,
+    10: Object,
+    11: Object,
+    12: Object
 });
 
 var PlayerRanking = mongoose.model('PlayerRanking', PlayerRankingSchema);
 
 router.get('/rankings', function(req, res, next) {
-  PlayerRanking.find(function(err, playerRanking){
-    if(err){ return next(err); }
-
-    res.json(playerRanking);
-  });
+    PlayerRanking.find(function(err, playerRanking){
+        if(err){ return next(err); }
+        res.json(playerRanking);
+    });
 });
 
 router.post('/ranking', function(req, res, next) {
 
-  	var playerRanking = new PlayerRanking(req.body);
-  	//playerRanking[1] = "VITHUSHAN";
-  	//playerRanking[2] = "ANU";
-	console.log("VITHUSHAN " + JSON.stringify(playerRanking) + "\n");
-  	playerRanking.save(function(err, playerRanking){
-  		console.log("LUCKSSON" + err);
-    	if(err){ return next(err); 
-	}
-
+    var playerRanking = new PlayerRanking(req.body);	
+    
+    playerRanking.save(function(err, playerRanking){
+        if(err){ return next(err); 
+    }
+    
     res.json(playerRanking);
-  });
+    });
 });
 
 module.exports = router;
