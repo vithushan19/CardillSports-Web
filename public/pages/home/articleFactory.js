f5fb752c0cbe42b1d050db808088ed644062f398
Vithushan Nama <vithushan@Vithushans-MacBook-Pro.local>
2016-06-10 19:28:19 -0400

    redesign

--- public/pages/home/articleFactory.js
+++ public/pages/home/articleFactory.js
@@ -1,12 +1,12 @@
 angular.module('cardillApp').factory('articles', [function(){
     var o = {
         articles: [
-            {title: 'Not So Fast', author: 'Zen Potter', imgPath: 'images/not-so-fast.jpg', link: '#/posts/not-so-fast'},
-            {title: 'F*** Clevland', author: 'Lucksson Nama', imgPath: 'images/f-clevland.png', link: '#/posts/f-clevland'},
-            {title: 'Draft Retrospective', author: 'Vithushan Nama', imgPath: 'images/draft-retro.png', link: '#/posts/draft-retro'},
-            {title: 'Round 2 Game 7', author: 'Vithushan Nama', imgPath: 'images/rd2-gm7.png', link: '#/posts/rd2-gm7'},	 
-            {title: 'Power of Veto', author: 'Vithushan Nama', imgPath: 'images/power-of-veto.jpg', link: '#/posts/power-of-veto'},
-            {title: 'The Day I Said What If', author: 'Zen Potter', imgPath: 'images/the-day-i-said-what-if.png', link: '#/posts/the-day-i-said-what-if'},	 
+            {title: 'Not So Fast', author: 'Zen Potter', authorImg: 'images/sujee.png', imgPath: 'images/not-so-fast.jpg', link: '#/posts/not-so-fast'},
+            {title: 'F*** Clevland', author: 'Lucksson Nama', authorImg: 'images/lucksson.png', imgPath: 'images/f-clevland.png', link: '#/posts/f-clevland'},
+            {title: 'Draft Retrospective', author: 'Vithushan Nama', authorImg: 'images/vithushan.png', imgPath: 'images/draft-retro.png', link: '#/posts/draft-retro'},
+            {title: 'Round 2 Game 7', author: 'Vithushan Nama', authorImg: 'images/vithushan.png', imgPath: 'images/rd2-gm7.png', link: '#/posts/rd2-gm7'},	 
+            {title: 'Power of Veto', author: 'Vithushan Nama', authorImg: 'images/vithushan.png', imgPath: 'images/power-of-veto.jpg', link: '#/posts/power-of-veto'},
+            {title: 'The Day I Said What If', author: 'Zen Potter', authorImg: 'images/sujee.png', imgPath: 'images/the-day-i-said-what-if.png', link: '#/posts/the-day-i-said-what-if'},	 
         ]
     };
     return o;
