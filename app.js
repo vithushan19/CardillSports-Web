3607c6d242f2be16d86a84ba77f33a1a77d170c3
Vithushan Nama <vithushan@Vithushans-MacBook-Pro.local>
2016-06-20 18:38:07 -0400

    add request var

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var request = require('request');
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
